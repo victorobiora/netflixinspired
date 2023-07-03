e236d0e54d4989f72def1c20cf191c910e6a3c81
Victor <victorobioratech@gmail.com>
2023-07-03 22:51:35 +0100

    testing in BrowseComponent

--- src/components/BrowseComponent.jsx
+++ src/components/BrowseComponent.jsx
@@ -1,8 +1,7 @@
 import classes from "./BrowseComponent.module.css";
-import NavBar from "./NavBar";
 import FeatureMovie from "./FeatureMovie";
 import CustomCarousel from "./CustomCarousel";
-import Footer from "./footer";
+
 import { useSelector } from "react-redux";
 
 const BrowseComponent = (props) => {
@@ -22,7 +21,7 @@ const BrowseComponent = (props) => {
         return <CustomCarousel carouselData={el} key={el.categoryName} />;
       })}
        <CustomCarousel carouselData={picked} key={picked.categoryName} />
-      <Footer />
+
     </div>
   );
 };
