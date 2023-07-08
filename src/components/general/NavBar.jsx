87f745134f8f70f0d61c053421d65f6545860518
Victor <victorobioratech@gmail.com>
2023-07-08 23:36:07 +0100

    NavBar background worked on

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -1,38 +1,11 @@
 import Link from "next/link";
 import classes from "./NavBar.module.css";
-import { useState, useEffect } from "react";
+
 
 const NavBar = (props) => {
 
-  const [navScroll, setNavScroll] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 0;
-      console.log(isScrolled)
-      setNavScroll(isScrolled);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const joinedClasses = `${classes.nav} ${classes.scrolled}`
-/* <li>
-          <Link href='/'>Movies</Link>
-          </li>
-          <li>
-          <Link href='/'>Tv Shows</Link>
-          </li>
-          <li>
-          <Link href='/'>New & Popular</Link>
-          </li>*/
-
   return (
-    <section className={navScroll ? joinedClasses : classes.nav}>
+    <section className={classes.nav}>
       <div className={classes.mainDeets}>
         <Link href="/" className={classes.home}>
           <h1>NETFLIX</h1>
