d8f191d3ca2b50ad5d502b9c0576dd176cd0deb8
Victor Obiora <victorobioratech@gmail.com>
2024-07-17 12:27:39 +0100

    merge conflict attended to

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import classes from "./NavBar.module.css";
-import { useRef } from "react";
-import { useRouter } from "next/router";
-import { useDispatch } from "react-redux";
-import { listActions } from "@/store/nStore";
-
-const NavBar = (props) => {
-  const searchValue = useRef();
-  const Router = useRouter();
-  const dispatch = useDispatch();
-
-  const searchHandler = el => {
-    el.preventDefault()
-    Router.push(`/search/${searchValue.current.value}`)
-  }
-  return (
-    <section className={classes.nav}>
-      <div className={classes.mainDeets}>
-        <Link href="/" className={classes.home}>
-          <h1>NETFLIX</h1>
-        </Link>
-        <ul className={classes.navBarDeets}>
-          <li>
-            <Link href="/browse/my-list">My List</Link>
-          </li>
-          <li>
-            <Link href="/browse">Browse</Link>
-          </li>
-        </ul>
-      </div>
-      <div className={classes.profileDeets}>
-        <form onSubmit={searchHandler}>
-          <input className={classes.searchInput} type="text" placeholder="Search" ref={searchValue}/>
-        </form>
-        <Link href="/">{props.signedInName}</Link>
-        <Link href="/login" onClick={(event) => {
-          
-           event.preventDefault()
-           dispatch(listActions.clearList())       
-           Router.push('/login')
-        }}>Log Out</Link>
-      </div>
-    </section>
-  );
-};
-
-export default NavBar;
-=======
 import Link from "next/link";
 import classes from "./NavBar.module.css";
 import { useRef } from "react";
@@ -88,4 +38,3 @@ const NavBar = (props) => {
 };
 
 export default NavBar;
->>>>>>> f927971 (netflix bugs fixed)
